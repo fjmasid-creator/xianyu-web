1b1a2566cb2130936ad2e0c0cee7de55f79a49f8
BHGA <bhga@email.com>
2026-03-22 22:20:42 +0800

    Fix CORS policy

--- server.js
+++ server.js
@@ -63,7 +63,11 @@ async function connectDB() {
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
